3502d6fc72cd0121d70bc3c494e5755d4d2f1cd7
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-24 08:05:15 +0300

    GlassContent is done;

--- lib/sections/topSection/top_section.dart
+++ lib/sections/topSection/top_section.dart
@@ -3,11 +3,14 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:portfolio_web/constants.dart';
 
+import 'components/glass_content.dart';
+
 class TopSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Container(
+      alignment: Alignment.center,
       constraints: BoxConstraints(
         maxHeight: 900,
         minHeight: 700,
@@ -24,10 +27,9 @@ class TopSection extends StatelessWidget {
       child: Container(
         width: 1200,
         margin: EdgeInsets.only(top: kDefaultPadding),
-        child: Column(
+        child: Stack(
           children: [
-            Image.asset('assets/images/logo.png'),
-            GlassContent(size: size)
+            LogoAndBlurBox(size: size),
           ],
         ),
       ),
@@ -35,8 +37,8 @@ class TopSection extends StatelessWidget {
   }
 }
 
-class GlassContent extends StatelessWidget {
-  const GlassContent({
+class LogoAndBlurBox extends StatelessWidget {
+  const LogoAndBlurBox({
     super.key,
     required this.size,
   });
@@ -45,18 +47,16 @@ class GlassContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-        child: Container(
-          constraints: BoxConstraints(
-            maxWidth: 1110,
-            maxHeight: size.height * 0.7,
-          ),
-          color: Colors.white.withOpacity(0),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Image.asset('assets/images/logo.png'),
+        Spacer(),
+        GlassContent(size: size),
+        Spacer(
+          flex: 3,
         ),
-      ),
+      ],
     );
   }
 }
