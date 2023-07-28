e2e9b6fef1269aaeb23cef237f1e6847c330e0a2
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-28 12:36:15 +0300

    The UI of FeedbackSection is finished .

--- lib/sections/feedback/feedback_section.dart
+++ lib/sections/feedback/feedback_section.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../../components/section_title.dart';
 import '../../constants.dart';
+import '../../models/Feedback.dart';
 import 'components/feedback_card.dart';
 
 class FeedbackSection extends StatelessWidget {
@@ -10,7 +12,21 @@ class FeedbackSection extends StatelessWidget {
       padding: EdgeInsets.symmetric(vertical: kDefaultPadding * 2.5),
       constraints: BoxConstraints(maxWidth: 1110),
       child: Column(
-        children: [],
+        children: [
+          SectionTitle(
+            title: "Feedback Received",
+            subTitle: "Client’s testimonials that inspired me a lot",
+            color: Color(0xFF00B1FF),
+          ),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: List.generate(
+              feedbacks.length,
+              (index) => FeedbackCard(index: index),
+            ),
+          ),
+        ],
       ),
     );
   }
