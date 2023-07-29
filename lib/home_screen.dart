b84ab8ce436c1e5d6aba002334f8488f63854fb1
Mahmoud <mahmoudabohebil@gmail.com>
2023-07-29 04:08:55 +0300

    Creating the ContactSection.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,9 @@ import 'package:portfolio_web/sections/recent_work/recent_work_section.dart';
 import 'package:portfolio_web/sections/service/service_section.dart';
 import 'package:portfolio_web/sections/topSection/top_section.dart';
 
+import 'constants.dart';
+import 'contact/contact_section.dart';
+
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,6 +20,8 @@ class HomeScreen extends StatelessWidget {
             ServiceSection(),
             RecentWorkSection(),
             FeedbackSection(),
+            SizedBox(height: kDefaultPadding),
+            ContactSection(),
             SizedBox(
               height: 500,
             ),
